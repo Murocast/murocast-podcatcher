961937b2302200d54ad724aa97c0069bff2e6e22
Sebastian Brandt <bastibrandt@gmail.com>
2017-11-22 21:33:40 +0100

    example for usign System.Net.HttpClient

--- src/Castos.Podcatcher.Application/Program.fs
+++ src/Castos.Podcatcher.Application/Program.fs
@@ -1,6 +1,18 @@
 ﻿open System
 
+let getAsync (url:string) =
+    async {
+        let httpClient = new System.Net.Http.HttpClient()
+        let! response = httpClient.GetAsync(url) |> Async.AwaitTask
+        response.EnsureSuccessStatusCode () |> ignore
+        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        return content
+    }
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    let content = getAsync "http://example.org/"
+                  |> Async.RunSynchronously
+    printfn "%s" content
     0 // return an integer exit code
