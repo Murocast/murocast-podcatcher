0e028befb0e2cb1ab9813cb74095d696cd1e97b0
Sebastian Brandt <bastibrandt@gmail.com>
2017-12-03 15:17:30 +0100

    reads episodes and adds them to castos

--- src/Castos.Podcatcher.Application/Program.fs
+++ src/Castos.Podcatcher.Application/Program.fs
@@ -1,6 +1,8 @@
 ﻿open System
 open Castos.Podcatcher.Json
 open FeedParserCore
+open System.Net.Http
+open System.Text
 
 type SubscriptionId = Guid
 type SubscriptionListItemRendition = {
@@ -9,6 +11,21 @@ type SubscriptionListItemRendition = {
     Name: string
     Category: string
     EpidsodesAmount: int
+}
+
+type EpisodeId = int
+type Episode = {
+    Id: EpisodeId
+    SubscriptionId: SubscriptionId
+    MediaUrl: string
+    Title: string
+    ReleaseDate: System.DateTime
+}
+
+type AddEpisodeRendition = {
+    Title: string
+    Url: string
+    ReleaseDate: System.DateTime
 }
 
 let getAsync (url:string) =
@@ -20,27 +37,47 @@ let getAsync (url:string) =
         return content
     }
 
+let postAsync (url:string) body =
+    async {
+        let content = new StringContent(body, Encoding.UTF8, "application/json");
+        let httpClient = new System.Net.Http.HttpClient()
+        let! response = httpClient.PostAsync(url, content) |> Async.AwaitTask
+        response.EnsureSuccessStatusCode () |> ignore
+
+        return! response.Content.ReadAsStringAsync() |> Async.AwaitTask
+    }
+
 let addEpisodeToSubscriptionAgent = MailboxProcessor.Start(fun inbox ->
     let rec loop() = async {
-        let! msg = inbox.Receive()
-        //TODO: Post to castos AddEpisode
+        let! (s:SubscriptionListItemRendition), (e: FeedItem) = inbox.Receive()
+        let rendition = { Title = e.Title
+                          Url = e.Link
+                          ReleaseDate = e.PublishDate }
+        let json = mkjson rendition
+        let! _ = postAsync (sprintf "http://localhost/api/subscriptions/%O/episodes" s.Id) json
+
+        printfn "Add episode %s to subscription %s" e.Link s.Name
         return! loop()
     }
 
     loop()
 )
 
-let updateSubscriptionAgent = MailboxProcessor.Start(fun inbox -> 
+let updateSubscriptionAgent = MailboxProcessor.Start(fun inbox ->
     let rec loop() = async {
-        let! msg = inbox.Receive()
+        let! (msg: SubscriptionListItemRendition) = inbox.Receive()
         let url = msg.Url
 
-        //TODO get existing Episodes for subscription
+        let content = getAsync (sprintf "http://localhost/api/subscriptions/%O/episodes" msg.Id )
+                      |> Async.RunSynchronously
 
-        let items = FeedParser.Parse(url, FeedType.RSS)
-                    |> List.ofSeq
-                    |> List.filter (fun item -> true) //TODO: Filter for new Episodes (by url)
-                    |> List.map (fun item -> addEpisodeToSubscriptionAgent.Post (msg, item))
+        let (episodes:Episode list) = unjson content
+
+        FeedParser.Parse(url, FeedType.Atom)
+        |> List.ofSeq
+        |> List.filter (fun item -> not(episodes |> List.exists (fun e -> item.Link.ToLowerInvariant() = e.MediaUrl.ToLowerInvariant())))
+        |> List.map (fun item -> addEpisodeToSubscriptionAgent.Post (msg, item))
+        |> ignore
 
         return! loop()
     }
@@ -50,12 +87,14 @@ let updateSubscriptionAgent = MailboxProcessor.Start(fun inbox ->
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
     let content = getAsync "http://localhost/api/subscriptions"
                   |> Async.RunSynchronously
 
     unjson content
     |> List.map updateSubscriptionAgent.Post
+    |> ignore
+
+    Console.ReadKey()
     |> ignore
 
     0 // return an integer exit code
