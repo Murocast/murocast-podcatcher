22c21807429fd423b5b70aa18f5a1481ab25e674
Sebastian Brandt <bastibrandt@gmail.com>
2018-01-03 23:26:56 +0100

    implemented a supervisor but not working right now. Queue gets not emtpy.

--- src/Castos.Podcatcher.Application/Program.fs
+++ src/Castos.Podcatcher.Application/Program.fs
@@ -5,6 +5,19 @@ open System.Text
 
 open Rss
 
+type Queue<'a>(xs : 'a list, rxs : 'a list) =
+    new() = Queue([], [])
+    static member Empty() = new Queue<'a>([], [])
+
+    member q.IsEmpty = (List.isEmpty xs) && (List.isEmpty rxs)
+    member q.Enqueue(x) = Queue(xs, x::rxs)
+    member q.TryTake() =
+        if q.IsEmpty then None, q
+        else
+            match xs with
+            | [] -> (Queue(List.rev rxs,[])).TryTake()
+            | y::ys -> Some(y), (Queue(ys, rxs))
+
 type SubscriptionId = Guid
 type SubscriptionListItemRendition = {
     Id: SubscriptionId
@@ -34,6 +47,25 @@ type AddEpisodeRendition = {
     MediaUrl: string
     Length: int
 }
+
+type SupervisorMessage =
+    | Start of MailboxProcessor<SupervisorMessage> * SubscriptionListItemRendition list * AsyncReplyChannel<unit>
+    | FetchCompleted
+
+type SupervisorProgress =
+    {
+        Supervisor : MailboxProcessor<SupervisorMessage>
+        ReplyChannel : AsyncReplyChannel<unit>
+        Workers : MailboxProcessor<SubscriptionListItemRendition> list
+        PendingUrls : SubscriptionListItemRendition Queue
+        Completed : SubscriptionListItemRendition list
+        Dispatched : int
+    }
+
+type SupervisorStatus =
+    | NotStarted
+    | Running of SupervisorProgress
+    | Finished
 
 [<Literal>]
 let CastosApi = "http://192.168.178.42/api"
@@ -70,7 +102,7 @@ let addEpisodeToSubscriptionAgent = MailboxProcessor.Start(fun inbox ->
     loop()
 )
 
-let updateSubscriptionAgent = MailboxProcessor.Start(fun inbox ->
+let updateSubscriptionAgent (supervisor : MailboxProcessor<SupervisorMessage>) = MailboxProcessor.Start(fun inbox ->
     let rec loop() = async {
         let! (msg: SubscriptionListItemRendition) = inbox.Receive()
         let url = msg.Url
@@ -94,12 +126,91 @@ let updateSubscriptionAgent = MailboxProcessor.Start(fun inbox ->
             addEpisodeToSubscriptionAgent.Post (msg, rendition))
         |> ignore
 
+        //TODO: The agents to add episodes have to be tracked aswell
+
+        supervisor.Post(FetchCompleted)
+
         return! loop()
     }
 
     loop()
 )
 
+let rec dispatch progress =
+    match progress.PendingUrls.TryTake() with
+    | None, _ -> progress
+    | Some subscription, queue -> match progress.Workers |> List.tryFind (fun worker -> worker.CurrentQueueLength = 0) with
+                                  | Some idleWorker ->
+                                        idleWorker.Post(subscription)
+                                        dispatch { progress with
+                                                       PendingUrls = queue
+                                                       Dispatched = progress.Dispatched + 1 }
+                                  | None when progress.Workers.Length < 5 ->
+                                        let newWorker = updateSubscriptionAgent progress.Supervisor
+                                        dispatch { progress with Workers = newWorker :: progress.Workers }
+                                  | _ -> dispatch progress
+
+let start supervisor replyChannel =
+    {
+        Supervisor = supervisor
+        ReplyChannel = replyChannel
+        Workers = []
+        PendingUrls = Queue.Empty()
+        Completed = []
+        Dispatched = 0
+    }
+
+let enqueueSubscriptions subscriptions progress =
+    let pending = progress.PendingUrls |> List.foldBack(fun url pending -> pending.Enqueue(url)) subscriptions
+    { progress with PendingUrls = pending }
+
+let handleStart supervisor subscriptions replyChannel =
+    let progress = start supervisor replyChannel
+                   |> enqueueSubscriptions subscriptions
+                   |> dispatch
+    if progress.PendingUrls.IsEmpty then
+        Finished
+    else
+        Running progress
+
+let complete progress =
+    { progress with
+        Dispatched = progress.Dispatched - 1 }
+
+let handleFetchCompleted progress =
+    let progress =
+        progress
+        |> complete
+    if progress.PendingUrls.IsEmpty && progress.Dispatched = 0 then
+        progress.ReplyChannel.Reply(())
+        Finished
+    else
+        Running progress
+
+let handleSupervisorMessage message state =
+    match message with
+    | Start (supervisor, subscriptions, replyChannel) ->
+        match state with
+        | NotStarted ->
+            handleStart supervisor subscriptions replyChannel
+        | _ -> failwith "Invalid state: Can't be started more than once."
+    | FetchCompleted ->
+        match state with
+        | Running progress ->
+            handleFetchCompleted progress
+        | _ -> failwith "Invalid state - can't complete fetch before starting."
+
+let updateSubscriptions subscriptions =
+    let supervisor = MailboxProcessor<SupervisorMessage>.Start(fun inbox ->
+        let rec loop state =
+            async {
+                let! message = inbox.Receive()
+                match state |> handleSupervisorMessage message with
+                | Finished -> return ()
+                | newState -> return! loop newState
+            }
+        loop NotStarted)
+    supervisor.PostAndAsyncReply(fun replyChannel -> Start(supervisor, subscriptions, replyChannel))
 
 [<EntryPoint>]
 let main argv =
@@ -107,10 +218,7 @@ let main argv =
                   |> Async.RunSynchronously
 
     unjson content
-    |> List.map updateSubscriptionAgent.Post
-    |> ignore
-
-    Console.ReadKey()
-    |> ignore
+    |> updateSubscriptions
+    |> Async.RunSynchronously
 
     0 // return an integer exit code
