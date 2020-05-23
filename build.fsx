7420ba3600e64ff287704d8750676b059ba4b082
brase <bastibrandt@posteo.de>
2020-05-23 20:54:18 +0200

    fake dotnet build

--- build.fsx
+++ build.fsx
@@ -1,13 +1,26 @@
 #r "paket:
+nuget Fake.Core.Target
+nuget Fake.Core.Process
+nuget Fake.DotNet.Cli
+nuget Fake.Core.ReleaseNotes
+nuget Fake.DotNet.AssemblyInfoFile
+nuget Fake.DotNet.Paket
+nuget Fake.Tools.Git
+nuget Fake.Core.Environment
+nuget Fake.Core.UserInput
 nuget Fake.IO.FileSystem
-nuget Fake.DotNet.MSBuild
-nuget Fake.Core.Target //"
+nuget Fake.DotNet.MsBuild
+nuget Fake.Api.GitHub //"
 #load "./.fake/build.fsx/intellisense.fsx"
 
-open Fake.IO
-open Fake.IO.Globbing.Operators //enables !! and globbing
-open Fake.DotNet
 open Fake.Core
+open Fake.DotNet
+open Fake.Tools
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+open Fake.Api
 
 let buildDir = "./build/"
 
@@ -15,14 +28,25 @@ Target.create "Clean" (fun _ ->
   Shell.cleanDir buildDir
 )
 
-Target.create "Build" (fun _ ->
-    !! "src/**/*.fsproj"
-      |> MSBuild.runRelease id buildDir "Build"
-      |> Trace.logItems "AppBuild-Output: "
+Target.create "Restore" (fun _ ->
+    DotNet.restore id ""
 )
 
-Target.create "Deploy" (fun _ ->
-  Trace.log " --- Deploying app --- "
+Target.create "Build" (fun _ ->
+    DotNet.build id ""
+)
+
+// --------------------------------------------------------------------------------------
+// Release Targets
+// --------------------------------------------------------------------------------------
+Target.create "BuildRelease" (fun _ ->
+    DotNet.build (fun p ->
+        { p with
+            Configuration = DotNet.BuildConfiguration.Release
+            OutputPath = Some buildDir
+            //MSBuildParams = { p.MSBuildParams with Properties = [("Version", release.NugetVersion); ("PackageReleaseNotes", String.concat "\n" release.Notes)]}
+        }
+    ) "Castos.Podcatcher.sln"
 )
 
 open Fake.Core.TargetOperators
@@ -30,7 +54,9 @@ open Fake.Core.TargetOperators
 // *** Define Dependencies ***
 "Clean"
   ==> "Build"
-  ==> "Deploy"
+
+"Clean"
+  ==> "BuildRelease"
 
 // *** Start Build ***
-Target.runOrDefault "Deploy"
+Target.runOrDefault "Build"
